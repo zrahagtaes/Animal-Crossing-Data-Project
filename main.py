08cf72c3a5c7b9f12cf388dfd0a042fe856f7efa
zrahagtaes <123510587+zrahagtaes@users.noreply.github.com>
2023-04-11 20:29:01 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ import plotly.express as px
 
 df = pd.read_csv('villagers.csv')
 df.head(20)
-
-
-
 df['Name'].count()
 
+Personalities = df.Personality.value_counts().plot(kind='pie', autopct='%.2f')
+plt.show()
+
 sns.catplot(x="Personality",  kind="count", hue='Gender', data=df)
+plt.xlabel("Different Personalities")
+plt.ylabel("Quantity")
 plt.show()
 
-Species = df.Species.value_counts().plot(kind = 'bar', color= "Blue", figsize = (12, 6))
+species = df.Species.value_counts().plot(kind='bar', color="Blue", figsize=(12,6))
 plt.xlabel("Different Species")
-plt.ylabel= ("quantity")
+plt.ylabel("Count")
 plt.show()
 
-
+compare = sns.catplot(x='Species', kind='count', hue='Personality',height=20, aspect=11.7/8.27,data = df)
+plt.legend(title_fontsize='400')
+plt.show()
